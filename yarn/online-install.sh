f83358e87ad9719c4a1260d838ab83b04e9f252f
Jamter Tsai <121978614@qq.com>
2019-09-05 17:11:04 +0800

    refactor: update online-install shell

--- yarn/online-install.sh
+++ yarn/online-install.sh
@@ -3,8 +3,7 @@
 SH_FILE_PATH=$(cd `dirname $0`; pwd) # $REPO_PATH/yarn
 cd $SH_FILE_PATH/../
 rm -rf node_modules/* yarn.lock yarn-error.log
-cd $SH_FILE_PATH
-npm install ./yarn-v1.17.3.tar.gz --no-save
+npm install $SH_FILE_PATH/yarn-v1.17.3.tar.gz --no-save
 npx yarn config set registry https://registry.npm.taobao.org
 npx yarn config set yarn-offline-mirror $SH_FILE_PATH/yarn-offline-mirror
 npx yarn config set yarn-offline-mirror-pruning false
