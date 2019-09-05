759d1ee96e32d93ab70fc911efe7be9c39f5e5ba
Jamter Tsai <121978614@qq.com>
2019-09-05 15:40:04 +0800

    fix: fix the problem that indirect dependencies not download in offline-mirror directory

--- yarn/online-install.sh
+++ yarn/online-install.sh
@@ -6,7 +6,7 @@ rm -rf node_modules/* yarn.lock yarn-error.log
 cd $SH_FILE_PATH
 npm install ./yarn-v1.17.3.tar.gz --no-save
 npx yarn config set registry https://registry.npm.taobao.org
-npx yarn config set yarn-offline-mirror $SH_FILE_PATH/node-modules-mirror
+npx yarn config set yarn-offline-mirror $SH_FILE_PATH/yarn-offline-mirror
 npx yarn config set yarn-offline-mirror-pruning false
 npx yarn cache clean
 npx yarn install
